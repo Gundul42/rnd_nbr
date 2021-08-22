626efe1c746018da0de5851cbf2ace4bed5fac7e
Gundul <sampah@mailbox.org>
2021-08-22 08:46:02 +0200

    test only eval like

--- testme.sh
+++ testme.sh
@@ -6,7 +6,7 @@
 #    By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/08/13 17:40:34 by graja             #+#    #+#              #
-#    Updated: 2021/08/22 08:32:03 by graja            ###   ########.fr        #
+#    Updated: 2021/08/22 08:43:55 by graja            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -27,8 +27,8 @@ START=$(date +%s)
 ../push_swap $TEST | wc -l
 END=$(date +%s)
 DIFF=$(( $END - $START ))
-echo "Push_swap needed $DIFF seconds"
 ../push_swap $TEST | ./checker $TEST
+echo "Push_swap needed $DIFF seconds"
 
 TEST=$(./rnd 5 1000 -1)
 echo
@@ -37,38 +37,8 @@ START=$(date +%s)
 ../push_swap $TEST | wc -l
 END=$(date +%s)
 DIFF=$(( $END - $START ))
-echo "Push_swap needed $DIFF seconds"
 ../push_swap $TEST | ./checker $TEST
-
-TEST=$(./rnd 10 1000 1)
-echo
-echo "Test with 10:"
-START=$(date +%s)
-../push_swap $TEST | wc -l
-END=$(date +%s)
-DIFF=$(( $END - $START ))
 echo "Push_swap needed $DIFF seconds"
-../push_swap $TEST | ./checker $TEST
-
-TEST=$(./rnd 30 1000 -1)
-echo
-echo "Test with 30:"
-START=$(date +%s)
-../push_swap $TEST | wc -l
-END=$(date +%s)
-DIFF=$(( $END - $START ))
-echo "Push_swap needed $DIFF seconds"
-../push_swap $TEST | ./checker $TEST
-
-TEST=$(./rnd 50 1000000 1)
-echo
-echo "Test with 50:"
-START=$(date +%s)
-../push_swap $TEST | wc -l
-END=$(date +%s)
-DIFF=$(( $END - $START ))
-echo "Push_swap needed $DIFF seconds"
-../push_swap $TEST | ./checker $TEST
 
 TEST=$(./rnd 100 10000000 -1)
 echo
@@ -77,8 +47,8 @@ START=$(date +%s)
 ../push_swap $TEST | wc -l
 END=$(date +%s)
 DIFF=$(( $END - $START ))
-echo "Push_swap needed $DIFF seconds"
 ../push_swap $TEST | ./checker $TEST
+echo "Push_swap needed $DIFF seconds"
 
 TEST=$(./rnd 500 100000000 1)
 echo
@@ -87,15 +57,5 @@ START=$(date +%s)
 ../push_swap $TEST | wc -l
 END=$(date +%s)
 DIFF=$(( $END - $START ))
-echo "Push_swap needed $DIFF seconds"
 ../push_swap $TEST | ./checker $TEST
-
-TEST=$(./rnd 1000 100000000 -1)
-echo
-echo "Test with 1000:"
-START=$(date +%s)
-../push_swap $TEST | wc -l
-END=$(date +%s)
-DIFF=$(( $END - $START ))
 echo "Push_swap needed $DIFF seconds"
-../push_swap $TEST | ./checker $TEST
