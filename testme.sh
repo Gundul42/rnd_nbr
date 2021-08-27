e0d4294e68f3d885085d9eea29e265207194c72f
Gundul <sampah@mailbox.org>
2021-08-27 15:25:07 +0200

    add bonus support

--- testme.sh
+++ testme.sh
@@ -6,7 +6,7 @@
 #    By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/08/13 17:40:34 by graja             #+#    #+#              #
-#    Updated: 2021/08/22 08:43:55 by graja            ###   ########.fr        #
+#    Updated: 2021/08/27 15:24:36 by graja            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,6 +20,14 @@ if [ ! -f ../push_swap ]; then
 	make -C ..
 fi
 
+if [ ! -f ../checker ]; then
+	make bonus -C ..
+fi
+
+if [ -f ../checker ]; then
+	BONUS=$"Y"
+fi
+
 TEST=$(./rnd 3 100 1)
 echo
 echo "Test with 3:"
@@ -28,6 +36,12 @@ START=$(date +%s)
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 ../push_swap $TEST | ./checker $TEST
+if [ -n "$BONUS" ]; then
+  echo "Your checker file: "
+  ../push_swap $TEST | ../checker $TEST
+else
+  echo "No Bonus available"
+fi
 echo "Push_swap needed $DIFF seconds"
 
 TEST=$(./rnd 5 1000 -1)
@@ -38,6 +52,12 @@ START=$(date +%s)
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 ../push_swap $TEST | ./checker $TEST
+if [ -n "$BONUS" ]; then
+  echo "Your checker file: "
+  ../push_swap $TEST | ../checker $TEST
+else
+  echo "No Bonus available"
+fi
 echo "Push_swap needed $DIFF seconds"
 
 TEST=$(./rnd 100 10000000 -1)
@@ -48,6 +68,12 @@ START=$(date +%s)
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 ../push_swap $TEST | ./checker $TEST
+if [ -n "$BONUS" ]; then
+  echo "Your checker file: "
+  ../push_swap $TEST | ../checker $TEST
+else
+  echo "No Bonus available"
+fi
 echo "Push_swap needed $DIFF seconds"
 
 TEST=$(./rnd 500 100000000 1)
@@ -58,4 +84,10 @@ START=$(date +%s)
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 ../push_swap $TEST | ./checker $TEST
+if [ -n "$BONUS" ]; then
+  echo "Your checker file: "
+  ../push_swap $TEST | ../checker $TEST
+else
+  echo "No Bonus available"
+fi
 echo "Push_swap needed $DIFF seconds"
